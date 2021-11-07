965b2e4fed75e21336fc785d1299264e0c0ab6aa
liuweiyibai <lw1140@163.com>
2021-11-07 14:55:50 +0800

    feat: 示例修改

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-  <VueSlider v-model="value" :width="400"></VueSlider>
+  <VueSlider
+    v-model="value"
+    :width="400"
+    :min="1"
+    :max="100"
+    :interval="1"
+    dragOnClick
+    :dotSize="25"
+    :lazy="true"
+  >
+    <template #dot="{value}">
+      {{ value }}
+    </template>
+  </VueSlider>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import VueSlider from '../lib/vue-slider';
+import { Options, Vue } from 'vue-class-component'
+import VueSlider from '../lib/vue-slider'
 
 @Options({
   components: {
@@ -12,7 +25,7 @@ import VueSlider from '../lib/vue-slider';
   },
 })
 export default class App extends Vue {
-  value = 40
+  value = [10, 20]
 }
 </script>
 
@@ -24,5 +37,15 @@ export default class App extends Vue {
   text-align: center;
   color: hsl(210, 29%, 24%);
   margin: 100px auto;
+}
+.vue-slider-dot {
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #3498db;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 500;
 }
 </style>
